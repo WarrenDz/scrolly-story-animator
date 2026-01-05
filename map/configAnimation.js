165b4c5b15c5aeb55b3a04ae02bc2bd4c4b2b4fd
Warren Davison <49045641+WarrenDz@users.noreply.github.com>
2026-01-05 16:13:48 -0500

    update map choreography

--- map/configAnimation.js
+++ map/configAnimation.js
@@ -3,7 +3,7 @@ export const animationConfig = {
   "itemType": "webmap",
   "itemId": "de29582d1c114221b571016576ab3f2f",
   "zoom": "4",
-  "center": "10.962341, 49.381060",
+  "center": "-119.57, 37.74",
   "timePlayRate": "2000",
   "debugMode": true,
   "disableMapNav": false,
